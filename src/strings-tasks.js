9e77e0c9a49269e3cc01abae6ff7542d8ccb178b
Miroslau <forse2408@gmail.com>
2026-09-15 15:50:15 +0300

    feat: removeFirstOccurrences function has been solved

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -104,8 +104,8 @@ function removeLeadingAndTrailingWhitespaces(value) {
  *   removeLeadingWhitespaces('cat ') => 'cat '
  *   removeLeadingWhitespaces('\t\t\tHello, World! ') => 'Hello, World! '
  */
-function removeLeadingWhitespaces(/* value */) {
-  throw new Error('Not implemented');
+function removeLeadingWhitespaces(value) {
+  return value.replace(/^\s+/, '');
 }
 
 /**
@@ -119,7 +119,7 @@ function removeLeadingWhitespaces(/* value */) {
  *   removeTrailingWhitespaces('cat ') => 'cat'
  *   removeTrailingWhitespaces('\t\t\tHello, World! ') => '\t\t\tHello, World!'
  */
-function removeTrailingWhitespaces(/* value */) {
+function removeTrailingWhitespaces(value) {
   throw new Error('Not implemented');
 }
 
@@ -136,8 +136,18 @@ function removeTrailingWhitespaces(/* value */) {
  *   repeatString('', 3) => ''
  *   repeatString('abc', -2) => ''
  */
-function repeatString(/* str, times */) {
-  throw new Error('Not implemented');
+function repeatString(str, times) {
+  let result = '';
+
+  if (str === '' || times < 0) {
+    return '';
+  }
+
+  for (let index = 0; index < times; index += 1) {
+    result += str;
+  }
+
+  return result;
 }
 
 /**
@@ -152,8 +162,19 @@ function repeatString(/* str, times */) {
  *   removeFirstOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeFirstOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeFirstOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+
+function removeFirstOccurrences(str, value) {
+  if (value === '') {
+    return str;
+  }
+
+  for (let index = 0; index < str.length; index += 1) {
+    if (str.startsWith(value, index)) {
+      return str.slice(0, index) + str.slice(index + value.length);
+    }
+  }
+
+  return str;
 }
 
 /**
